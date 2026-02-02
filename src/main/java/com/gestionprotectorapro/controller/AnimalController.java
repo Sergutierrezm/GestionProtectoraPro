a4d2c8a6f4c26e4ef379b3b2304ffb75ab87c57b
Sergutierrezm <sergiogutierrez370@gmail.com>
2026-02-02 19:07:31 +0100

    Añadido AnimalController: Listar animales no adoptados, actualizar animales, eliminar animales. Prueba postman ok

--- src/main/java/com/gestionprotectorapro/controller/AnimalController.java
+++ src/main/java/com/gestionprotectorapro/controller/AnimalController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/animals")
+
 public class AnimalController {
 
     private final AnimalRepository animalRepository;
@@ -22,11 +23,38 @@ public class AnimalController {
         return animalRepository.findAll();
     }
 
-    
+
     //Insertar
     @PostMapping
     public Animal insertarAnimal(@RequestBody Animal animal){
         return animalRepository.save(animal);
+    }
+
+    //Listar animales no adoptados
+
+    @GetMapping("/no-adoptados")
+    public List<Animal> listarNoAdoptados() {
+        return animalRepository.findByAdoptadoFalse();
+    }
+
+    //Actualizar animal
+    @PutMapping("/{id}")
+    public Animal actualizarAnimal(@PathVariable Long id, @RequestBody Animal animalActualizado) {
+        Animal animal = animalRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Animal no encontrado"));
+        animal.setNombre(animalActualizado.getNombre());
+        animal.setEspecie(animalActualizado.getEspecie());
+        animal.setEdad(animalActualizado.getEdad());
+        animal.setAdoptado(animalActualizado.isAdoptado());
+
+        return animalRepository.save(animal);
+    }
+
+    //Eliminar animal
+
+    @DeleteMapping("/{id}")
+    public void eliminarAnimal(@PathVariable Long id) {
+        animalRepository.deleteById(id);
     }
 
 }
