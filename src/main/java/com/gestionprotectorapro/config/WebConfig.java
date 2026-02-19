80a8e3ae03fc365ce71fa5bb9ce27b42d70d49c0
Sergutierrezm <sergiogutierrez370@gmail.com>
2026-02-20 00:22:21 +0100

    Añadido Pack config: webConfig.java - Configuracion CORS

---
+++ src/main/java/com/gestionprotectorapro/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.gestionprotectorapro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // permite todas las rutas
+                        .allowedOrigins("http://localhost:5173") // tu frontend React
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
